cee198ffe44b9f38b9c8dd2ae4be0e51ad0f7c26
RJ <rybertjen@gmail.com>
2026-08-14 05:19:55 -0700

    Run socket tests in isolated process

--- tests/main.test.cpp
+++ tests/main.test.cpp
@@ -11,15 +11,6 @@ using namespace coda::net;
 
 int main(int argc, char *argv[])
 {
-#ifdef CURL_FOUND
-    http::client::set_request_type(http::curl::request);
-
-    if (bandit::run(argc, argv)) {
-        return EXIT_FAILURE;
-    }
-#endif
-
     http::client::set_request_type(http::socket::request);
-
     return bandit::run(argc, argv);
 }
